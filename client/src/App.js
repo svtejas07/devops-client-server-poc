23201e92086ca5cdd5bcb40f5007bf72496f625d
Vishnu <svtejas04@gmail.com>
2024-08-30 22:39:47 +0530

    Added react file

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <p>Welcome to the DevOps PoC Client Application</p>
+    </div>
+  );
+}
+
+export default App;
